5e170bf5d41ed21924e8f745002c1141e718d216
karkisav <saurav.karki2@icloud.com>
2024-02-05 13:11:52 +0530

    Adding 2 more problem solutions

---
+++ lab1/2.c
@@ -0,0 +1,17 @@
+/*Write a C program to identify whether a given string is Palindrome or not using the stack.*/
+
+#include <stdio.h>
+#include <ctype.h>
+
+void push(char ch);
+void pop();
+void display();
+
+int main(void)
+{
+    char string[50];
+    printf("Enter your string: ")
+    scanf("%s", string);
+
+    
+}
