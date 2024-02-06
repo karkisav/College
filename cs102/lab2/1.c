effa6362556858c04216fae858ce1d2ab77660b2
karkisav <saurav.karki2@icloud.com>
2024-02-06 14:04:41 +0530

    ...

---
+++ cs102/lab2/1.c
@@ -0,0 +1,10 @@
+/*1.Write a C program to read an expression and print the precedence of the operators present in it
+using the stack data structure.*/
+
+#include <stdio.h>
+void pop();
+void display();
+int main(void)
+{
+
+}
