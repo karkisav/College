da40f2df32912b634e95a7bd8566ba697755484e
karkisav <saurav.karki2@icloud.com>
2024-02-14 10:27:16 +0530

    ...

---
+++ cs102/lecture/circll.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+bool check(int *array[]);
+
+typedef struct node
+{
+    int value;
+    struct node *next;
+}node;
+
+int main(void)
+{//😒
+    int count;s
+    for(ptr; ptr!)
+}
